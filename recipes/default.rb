3fbf451e371580f4b137bf4ccf01a0dc99612f9e
Jim Rosser <jarosser06@gmail.com>
2015-02-19 08:38:45 -0600

    fixed foodcritic issues

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,16 +9,16 @@ include_recipe 'apt::default'
 include_recipe 'ark'
 include_recipe 'java' if node['solr']['install_java']
 
-ark_prefix_root = node.solr[:dir] || node.ark[:prefix_root]
-ark_prefix_home = node.solr[:dir] || node.ark[:prefix_home]
+ark_prefix_root = node['solr']['dir']
+ark_prefix_home = node['solr']['dir']
 
 ark 'solr' do
   url node['solr']['url']
   checksum node['solr']['version_checksum']
   owner 'root'
-  version "#{node['solr']['version']}"
-  prefix_root ark_prefix_root
-  prefix_home ark_prefix_home
+  version node['solr']['version']
+  prefix_root ark_prefix_root unless ark_prefix_root.nil?
+  prefix_home ark_prefix_home unless ark_prefix_home.nil?
 end
 
 template '/var/lib/solr.start' do
@@ -27,8 +27,8 @@ template '/var/lib/solr.start' do
   group 'root'
   mode '0755'
   variables(
-    solr_dir: "#{node.solr[:dir]}/solr",
-    solr_home: "#{node.solr[:data_dir]}/solr",
+    solr_dir: "#{node['solr']['dir']}/solr",
+    solr_home: "#{node['solr']['data_dir']}/solr",
     port: node['solr']['port'],
     pid_file: node['solr']['pid_file'],
     log_file: node['solr']['log_file'],
@@ -43,8 +43,8 @@ template '/etc/init.d/solr' do
   group 'root'
   mode '0755'
   variables(
-    solr_dir: "#{node.solr[:dir]}/solr",
-    solr_home: "#{node.solr[:data_dir]}/solr",
+    solr_dir: "#{node['solr']['dir']}/solr",
+    solr_home: "#{node['solr']['data_dir']}/solr",
     port: node['solr']['port'],
     pid_file: node['solr']['pid_file'],
     log_file: node['solr']['log_file'],
